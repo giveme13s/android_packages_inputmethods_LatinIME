c4bb3dfec600c1356836cac6b7e11a1cead2af71
Alex <mazdarider23@gmail.com>
2013-12-16 15:41:31 -0600

    Revert "latinIME: fix NumberFormatException"
    This reverts commit 53c1e87738db4577d8f78e587a22eb875c63f1b1.

--- java/src/com/android/inputmethod/latin/settings/SettingsValues.java
+++ java/src/com/android/inputmethod/latin/settings/SettingsValues.java
@@ -357,7 +357,7 @@ public final class SettingsValues {
                     autoCorrectionThreshold = Float.MAX_VALUE;
                 } else if (FLOAT_NEGATIVE_INFINITY_MARKER_STRING.equals(val)) {
                     autoCorrectionThreshold = Float.NEGATIVE_INFINITY;
-                } else if (val != null && !val.isEmpty()) {
+                } else {
                     autoCorrectionThreshold = Float.parseFloat(val);
                 }
             } else {
